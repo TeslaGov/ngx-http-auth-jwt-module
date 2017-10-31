9927fb944ef25f3aaeb80f78fdb2bcbd62cc343a
Joseph Fitzgerald <joefitz@alumni.princeton.edu>
2017-10-31 13:21:30 -0400

    Corrected comments

--- src/ngx_http_auth_jwt_module.c
+++ src/ngx_http_auth_jwt_module.c
@@ -392,15 +392,11 @@ static ngx_table_elt_t* search_headers_in(ngx_http_request_t *r, u_char *name, s
 	ngx_table_elt_t            *h;
 	ngx_uint_t                  i;
 
-	/*
-	Get the first part of the list. There is usual only one part.
-	*/
+	// Get the first part of the list. There is usual only one part.
 	part = &r->headers_in.headers.part;
 	h = part->elts;
 
-	/* Headers list array may consist of more than one part,
-	* so loop through all of it
-	*/
+	// Headers list array may consist of more than one part, so loop through all of it
 	for (i = 0; /* void */ ; i++)
 	{
 		if (i >= part->nelts)
@@ -416,9 +412,7 @@ static ngx_table_elt_t* search_headers_in(ngx_http_request_t *r, u_char *name, s
 			i = 0;
 		}
 
-		/*
-		* Just compare the lengths and then the names case insensitively.
-		*/
+		//Just compare the lengths and then the names case insensitively.
 		if (len != h[i].key.len || ngx_strcasecmp(name, h[i].key.data) != 0)
 		{
 			/* This header doesn't match. */
@@ -426,9 +420,9 @@ static ngx_table_elt_t* search_headers_in(ngx_http_request_t *r, u_char *name, s
 		}
 
 		/*
-		* a-da, we got one!
-		* Note, we'v stop the search at the first matched header
-		* while more then one header may fit.
+		* Ta-da, we got one!
+		* Note, we've stopped the search at the first matched header
+		* while more then one header may match.
 		*/
 		return &h[i];
 	}
